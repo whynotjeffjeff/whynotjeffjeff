3225a58a3bbdf9540c4ae499ee60e52e683755b9
nthr3ads <ebxeax@hotmail.com>
2024-03-29 17:05:25 +0800

    run flash script

--- tableGen.py
+++ tableGen.py
@@ -13,7 +13,7 @@ dir = './posts/'
 name = os.listdir(dir)
 print(">>>File : " + str(len(name)) + " files")
 mdline = []
-url = 'https://github.com/nthr3ads/nthr3ads/blob/master/posts/'
+url = 'https://github.com/whynotjeffjeff/whynotjeffjeff/blob/master/posts/'
 for i in name:
     link = '[' + i.split('.md')[0] + '](' + url + i + ')'
     mdline.append("|" + t_c(dir + i) + "|" + t(dir + i) + "|" + link)
@@ -28,12 +28,12 @@ title = """
 
 rd0 = """# Welcome to my homepage
 
-![nthr3ads's GitHub stats](https://github-readme-stats.vercel.app/api?username=nthr3ads&count_private=true&theme=dark)
+![whynotjeffjeff's GitHub stats](https://github-readme-stats.vercel.app/api?username=whynotjeffjeff&count_private=true&theme=dark)
 
-![Top Langs](https://github-readme-stats.vercel.app/api/top-langs?username=nthr3ads&layout=compact&count_private=true&theme=dark)
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs?username=whynotjeffjeff&layout=compact&count_private=true&theme=dark)
 
 ## Website [temp use waiting for finishing post sys]
-- [https://nthr3ads.github.io](https://nthr3ads.github.io)
+- [https://whynotjeffjeff.github.io](https://whynotjeffjeff.github.io)
 
 ## Markdown Table 
 
--- treeGen.py
+++ treeGen.py
@@ -3,7 +3,7 @@ dir = './posts/'
 mds = os.listdir(dir)
 mds.sort()
 lis = []
-url = 'https://github.com/nthr3ads/nthr3ads/blob/master/md/'
+url = 'https://github.com/whynotjeffjeff/whynotjeffjeff/blob/master/md/'
 for i in mds:
     st = '- [' + i.split('.md')[0] + '](' + url + i + ')'
     lis.append(st)
@@ -11,12 +11,12 @@ lis.sort()
 
 rd0 = """# Welcome to my homepage
 
-![nthr3ads's GitHub stats](https://github-readme-stats.vercel.app/api?username=nthr3ads&count_private=true&theme=dark)
+![whynotjeffjeff's GitHub stats](https://github-readme-stats.vercel.app/api?username=whynotjeffjeff&count_private=true&theme=dark)
 
-![Top Langs](https://github-readme-stats.vercel.app/api/top-langs?username=nthr3ads&layout=compact&count_private=true&theme=dark)
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs?username=whynotjeffjeff&layout=compact&count_private=true&theme=dark)
 
 ## Website [temp use waiting for finishing post sys]
-- [https://nthr3ads.github.io](https://nthr3ads.github.io)
+- [https://whynotjeffjeff.github.io](https://whynotjeffjeff.github.io)
 
 
 ## Markdown Tree 
